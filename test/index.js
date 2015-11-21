01186d010289c9df748df1749a6282f0c768d622
Junle Li <lijunle@gmail.com>
2015-11-22 05:07:15 +0800

    Add the basic test code.
    - Always use `fsp.readFile` to get content.
    - Avoid `require` call because it has side-affected cache.

---
+++ test/index.js
@@ -0,0 +1,65 @@
+var test = require('ava');
+var path = require('path');
+var fsp = require('../src/fs-promise');
+var patchVersion = require('../src/index');
+
+function storage() {
+  var value = '';
+
+  function store(newValue) {
+    value = newValue;
+  }
+
+  Object.defineProperty(store, 'value', {
+    get: function get() {
+      return value;
+    },
+  });
+
+  return store;
+}
+
+function include(str, substr) {
+  return str.indexOf(substr) !== -1;
+}
+
+test(function testPatchVersion(t) {
+  var dir = path.resolve(__dirname, './fake');
+  var file = path.resolve(dir, './package.json');
+  var version = '0.0.2';
+  var log = storage();
+  var error = storage();
+  var exit = storage();
+
+  return fsp.readFile(file, 'utf-8')
+    .then(function backup(content) {
+      t.context.content = content;
+      return content;
+    })
+    .then(function prepareAct(content) {
+      var metadata = JSON.parse(content);
+      t.same(metadata.version, '0.0.1');
+      t.notSame(metadata.version, version);
+    })
+    .then(function act() {
+      return patchVersion(dir, version, log, error, exit);
+    })
+    .then(function prepareAssert() {
+      return fsp.readFile(file, 'utf-8');
+    })
+    .then(function assert(content) {
+      var metadata = JSON.parse(content);
+      t.same(metadata.version, version);
+      t.same(metadata.name, 'fake');
+      t.ok(include(log.value, 'version'));
+      t.ok(include(log.value, version));
+      t.ok(include(log.value, file));
+      t.ok(error.value === '');
+      t.ok(exit.value === 0);
+    })
+    .then(function restore() {
+      return fsp.writeFile(file, t.context.content);
+    }, function restore() {
+      return fsp.writeFile(file, t.context.content);
+    });
+});
